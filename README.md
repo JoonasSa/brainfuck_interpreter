078ad8a873e81e7811fe59d9e65f9367ab8b01df
JoonasSa <joonas.sarapalo@gmail.com>
2016-11-22 15:10:29 +0200

    Added file read

--- README.md
+++
@@ -1,5 +0,0 @@
-# brainfuck_interpreter
-
-**An interpreter for escentric programming language [Brainfuck](https://en.wikipedia.org/wiki/Brainfuck) written in C.**
-
-**Compile with gcc.**
--- main.c
+++ main.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void interprete_bf(char* pcntr);
+char* read_file(char* fileName);
+
 int main(int argc, char** argv) {
-    
-    //Brainfuck to C?
-    //file read https://tmc.mooc.fi/mooc/exercises/4861/solution kannattaa tehdä pathin kautta
-    //arg read: flagi spacejen ja /n poistamiselle ja/tai kaikille turhille merkeille?
+
     if (argc == 1) {
         printf("An argument is required.\n");
         return 0;
-    } else if (argc > 2) {
-        printf("Only one argument allowed.\n");
-        return 0;
     }
-    
+
+    if (strcmp(argv[1], "-f") == 0) {
+        interprete_bf(read_file(argv[2]));
+    } else {
+        interprete_bf(argv[1]);
+    }
+    printf("\n");
+    return 0;
+}
+
+void interprete_bf(char* pcntr) {
     char array[10000];
     char *arrptr = array;
-    printf("%c", *arrptr);
-    char *pcntr = argv[1];
     int bracketnum = 0;
-    
-    while(*pcntr) {
+
+    while (*pcntr) {
         switch (*pcntr) {
-            case '>': 
-                ++arrptr; 
+            case '>':
+                ++arrptr;
                 break;
-            case '<': 
-                --arrptr; 
+            case '<':
+                --arrptr;
                 break;
-            case '+': 
-                ++*arrptr; 
+            case '+':
+                ++*arrptr;
                 break;
-            case '-': 
-                --*arrptr; 
+            case '-':
+                --*arrptr;
                 break;
-            case '.': 
-                printf("%c", *arrptr); 
+            case '.':
+                printf("%c", *arrptr);
                 break;
-            case ',': 
-                scanf("%c", arrptr); 
+            case ',':
+                scanf("%c", arrptr);
                 break;
             case '[':
                 if (*arrptr != 0) break;
@@ -61,10 +66,26 @@ int main(int argc, char** argv) {
                 }
                 break;
             default:
+                if (*pcntr == '\n') break;
+                if (*pcntr == '\0') return;
                 printf("Input contains unknown character: %c\n", *pcntr);
-                return 0;
-        } 
+                return;
+        }
         pcntr++;
     }
-    return 0;
+}
+
+char* read_file(char* fileName) {
+    FILE *file = fopen(fileName, "r");
+    if (!file) {
+        printf("An error occurred while opening the file.\n");
+        exit(-1);
+    }
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* pcntr = malloc(sizeof(char) * length);
+    fread(pcntr, sizeof(char), length, file);
+    fclose(file);
+    return pcntr;
 }
